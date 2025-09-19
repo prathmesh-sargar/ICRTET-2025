f3ed4c6f782cd3131ee7fedd43e8f1a3b1875e11
prathmeshTech07005 <sargarprathmesh007@gmail.com>
2025-09-19 12:07:53 +0530

    update

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -47,13 +47,15 @@ export default function Hero() {
               In collaboration with AICTE and Elsevier
             </span>
           </p>
-          <div className="font-semibold italic">
+          <marquee className="font-semibold italic bg-blue-300 p-1.5" behavior="" direction="">
+          <div >
             " The Microsoft CMT service was used for managing the peer-reviewing
             process for this conference. This service was provided for free by
             Microsoft and they bore all expenses, including costs for Azure
             cloud services as well as for software development and support. "
           </div>
-          <br />
+          </marquee>
+          <br /><br />
           <div className="flex flex-col sm:flex-row gap-4 justify-center">
             <Link to={"submit-paper"}>
               <Button
